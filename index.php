47985e083f448ce78edcede55729b15f721831ae
Thinh <thinh@gmail.com>
2017-07-11 10:34:13 +0700

    Adding codes for index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php get_header(); ?>
 <div class="content">
-	<div class="container">
-		
-		<div id="sidebar" class="col-md-4">
-			<?php get_sidebar(); ?>
-		</div>
+	<div id="main-content" class="col-md-8">
+		<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+			<?php get_template_part('content',get_post_format()); ?>
+		<?php endwhile ?>
+		<?php else: ?>
+			<?php get_template_part('content','none'); ?>
+		<?php endif; ?>
+	</div>			
+	<div id="sidebar" class="col-md-4">
+		<?php get_sidebar(); ?>
 	</div>
 </div>
 </div><!-- end #container -->
