4cee8dd6e597187269d23b4ff5f964061f2bddaf
Thinh <thinh@gmail.com>
2017-07-11 09:55:25 +0700

    Add some codes for footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
-<footer id="footer">
-	<div class="copyright">
-		&copy; Copyright by Duc Thinh  <?php echo date('Y'); ?> - <?php bloginfo('sitename'); ?>
+<footer id="footer" class="text-center">
+	<div class="social-contact text-center">
+		<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+		<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+		<a href="#"><i class="fa fa-glide-g" aria-hidden="true"></i></a>
+		<a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>		
 	</div>
-</footer>
-
-</div>   <!-- end #container -->
+	<div class="copyright ">
+		<?php echo date('Y'); ?> Cherry. Designed with <?php echo '<i class="fa fa-heart-o" aria-hidden="true"></i>'; ?> by Duc Thinh
+	</div>
+</footer>  
 <?php wp_footer(); ?>
 </body>
 </html>
