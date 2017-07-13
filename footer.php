2f7306bdbd1802f506ffe0650293c730731b8b94
Thinh <thinh@gmail.com>
2017-07-13 15:06:21 +0700

    Adding and Style pre-footer part

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<div id="pre-footer" class="text-center">
+	<div class="logo-footer">
+		<img src="<?php echo get_template_directory_uri() ?>/logo-footer.png">
+		<h5>CHERRY RESTAURANT</h5>	
+	</div>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium laborum minus neque tempore accusamus non consequuntur officia cupiditate consequatur quia alias, dolorum placeat quis voluptatum asperiores sapiente deleniti? Laboriosam, possimus.</p>
+	<div class="more-info">
+		<a href=""><i class="fa fa-bookmark-o" aria-hidden="true"></i> Terms of Use</a>
+		<a href=""><i class="fa fa-question" aria-hidden="true"></i> FAQs</a>
+		<a href=""><i class="fa fa-asterisk" aria-hidden="true"></i> Privacy Policy</a>
+	</div>
+</div>
+
 <footer id="footer" class="text-center">
 	<div class="social-contact text-center">
 		<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
